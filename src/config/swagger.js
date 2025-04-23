47b2de9b1923e69427c68d714d24c332677e9d7e
glass <88633725+glass-source@users.noreply.github.com>
2025-04-23 14:48:38 -0400

    fix(cors) MORE attempts at fixing cors issues

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -13,7 +13,7 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: process.env.BASE_URL || 'http://localhost:3000',
+        url: process.env.BASE_URL || 'https://tecnitrama-api.onrender.com',
         description: 'Dynamic server',
       },
     ],
